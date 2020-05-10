f8caea20edcdad585f532c67539a8c35fa05902d
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2020-05-10 22:02:02 +0200

    Fix computer listing when changing sorting column

--- src/main/scala/com/github/pdalpra/computerdb/http/html/ComputersListView.scala
+++ src/main/scala/com/github/pdalpra/computerdb/http/html/ComputersListView.scala
@@ -94,7 +94,7 @@ object ComputersListView {
   private def pageLink(targetPage: Page.Number, context: Context, newSort: Option[ComputerSort], newOrder: Option[Order]) = {
     val sort    = newSort.getOrElse(context.sort)
     val order   = newOrder.getOrElse(context.order)
-    val baseUrl = s"/computers?p=$targetPage&n=${context.page}&s=${sort.value}&d=${order.entryName}"
+    val baseUrl = s"/computers?p=$targetPage&n=${context.pageSize}&s=${sort.value}&d=${order.entryName}"
     context.searchQuery.map(filter => baseUrl + s"&f=$filter").getOrElse(baseUrl)
   }
 
