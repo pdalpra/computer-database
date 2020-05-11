caefebc243b47e5d78b651d0e151ef534edb8875
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2020-05-11 17:39:21 +0200

    Move Circe Decoders to companion objects

--- src/main/scala/com/github/pdalpra/computerdb/model/Company.scala
+++ src/main/scala/com/github/pdalpra/computerdb/model/Company.scala
@@ -3,10 +3,13 @@ package com.github.pdalpra.computerdb.model
 import cats.implicits._
 import cats.{ derived, Eq }
 import eu.timepit.refined.cats._
+import io.circe.Decoder
+import io.circe.refined._
 
 object Company {
   object Id {
-    implicit val eq: Eq[Id] = derived.semi.eq
+    implicit val eq: Eq[Id]           = derived.semi.eq
+    implicit val decoder: Decoder[Id] = Decoder[UniqueId].map(Id.apply)
   }
   final case class Id(value: UniqueId) {
     override def toString: String = value.toString()
