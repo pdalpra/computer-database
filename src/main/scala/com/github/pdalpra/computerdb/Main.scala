a17ab9fa8b6f4427f738ef65b813dc37f4d5f77b
Pierre Dal-Pra <dalpra.pierre@gmail.com>
2020-05-26 00:09:38 +0200

    Update zio to 1.0.0-RC20, closes #97

--- src/main/scala/com/github/pdalpra/computerdb/Main.scala
+++ src/main/scala/com/github/pdalpra/computerdb/Main.scala
@@ -1,13 +1,11 @@
 package com.github.pdalpra.computerdb
 
-import cats.effect.ExitCode
 import zio._
 import zio.interop.catz._
 import zio.interop.catz.implicits._
 
 object Main extends CatsApp {
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
-    new ComputerDatabase[Task].program
-      .fold(_ => ExitCode.Error, _ => ExitCode.Success)
-      .map(_.code)
+
+  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
+    new ComputerDatabase[Task].program.exitCode
 }
